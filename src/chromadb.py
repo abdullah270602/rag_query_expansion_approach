3a88d025a35444fda8e6845a44b39f7f38df8fbb
Abdullah <Abdullahnaseem27@gmail.com>
2025-01-09 03:02:00 +0500

    Rename embedder.py to chromadb.py and add document retrieval functionality

--- src/chromadb.py
+++ src/chromadb.py
@@ -17,3 +17,10 @@ def add_to_collection(collection,documents):
     collection.add(ids=[str(i) for i in range(len(documents))], documents=documents)
     
     return collection
+
+
+def retriveved_documents(collection,query,n_results=3):
+    """ Query the collection with the query """
+    results = collection.query(query_texts=[query], n_results=n_results)
+    
+    return results
